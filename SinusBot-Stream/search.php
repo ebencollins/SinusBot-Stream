2c4e6a18e70fbca659c8851fe29e8438012fd389
Zahzi <collinseben@gmail.com>
2016-09-04 21:58:29 -0400

    Update search.php

--- SinusBot-Stream/search.php
+++ SinusBot-Stream/search.php
@@ -1,9 +1,8 @@
 <?php
 error_reporting('E_ERROR');
 include("getSong.php");
-
 if(!empty($artist)) {
-echo "<font color='black' style='size: 16px !important;'>Song: <a href='https://www.google.de/search?client=opera&q=".getTrack()."+-+".getArtist()."&sourceid=opera&ie=UTF-8&oe=UTF-8' target='_new' style='text-decoration: none;''>".getName() . " from " . artist() . "</a> </font>";
+  echo "<font color='black' style='size: 16px !important;'>Song: <a href='https://www.google.de/search?client=opera&q=".getTrack()."+-+".getArtist()."&sourceid=opera&ie=UTF-8&oe=UTF-8' target='_new' style='text-decoration: none;''>".getName() . " from " . artist() . "</a> </font>";
 } else {
-echo "<font color='black' style='size: 16px !important;'>Song: <a href='https://www.google.de/search?client=opera&q=".getTrack()."&sourceid=opera&ie=UTF-8&oe=UTF-8' target='_new' style='text-decoration: none;''>".getName()."</a></font>";
+  echo "<font color='black' style='size: 16px !important;'>Song: <a href='https://www.google.de/search?client=opera&q=".getTrack()."&sourceid=opera&ie=UTF-8&oe=UTF-8' target='_new' style='text-decoration: none;''>".getName()."</a></font>";
 }
