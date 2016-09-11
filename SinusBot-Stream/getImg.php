1efa485066df60acfa74e9529e87adae50b4d147
Zahzi <collinseben@gmail.com>
2016-09-11 19:56:48 -0400

    Improved method for getting youtube thumbnails from metadata +Now includes a proper validateURL method   +Checks to see if URL leads to somewhere invalid (404)   +Prepends "http://" to URL if it isn't there to pass the validation *getYoutubeID now uses url_parse, and should work more of the time

--- SinusBot-Stream/getImg.php
+++ SinusBot-Stream/getImg.php
@@ -2,21 +2,32 @@
 error_reporting('E_ERROR');
 include("getSong.php");
 
-$finalURL = "resources/unknownimg.png";
+$unknownimg = "resources/unknownimg.png";
+$finalURL = $unknownimg;
 
-if($findThumbnailFromMetaData && (($urlFromMD = checkMetaDataForURL()) !== false)){ //there is a URL and this search method is enabled
-    if(($urlFull = resolveURL($urlFromMD)) !== false){ //it can be resolved to something
-        if(($ytID = getYoutubeID($urlFull)) !== false){ //it's a youtube link
-          $finalURL = "https://i.ytimg.com/vi/". $ytID ."/sddefault.jpg";
-        }
-    }
-}else if($searchForThumbnail && $finalURL == "resources/unknownimg.png"){
-//implement at some point
-}else if($useCachedThumbnail && ($finalURL == "resources/unknownimg.png")){
-    if(array_key_exists('thumbnail', $status['currentTrack'])){
-        $thumbnailURL = "http://" . $ipport . "/cache/" . $status['currentTrack']['thumbnail'];
-        $finalURL = $thumbnailURL;
-    }
+if($findThumbnailFromMetaData){
+	if(($urlFromMD = checkMetaDataForURL()) !== false){ //metdata contains a link
+		if(($urlFull = resolveURL($urlFromMD)) !== false){ //it is a valid url and has been resolved to a full URL
+	        if(($ytID = getYoutubeID($urlFull)) !== false){ //it's a youtube link
+	        $finalURL = "https://i.ytimg.com/vi/". $ytID ."/sddefault.jpg";
+	    }else{
+	    	echo "3";
+	    }
+	}else{
+		echo "2";
+	}
+}else{
+	echo "1";
+}
+}
+if($searchForThumbnail && $finalURL == $unknownimg){
+// implement at some point
+}
+if($useCachedThumbnail && ($finalURL == $unknownimg)){
+	if(array_key_exists('thumbnail', $status['currentTrack'])){
+		$thumbnailURL = "http://" . $ipport . "/cache/" . $status['currentTrack']['thumbnail'];
+		$finalURL = $thumbnailURL;
+	}
 }
 echo $finalURL;
 
--- SinusBot-Stream/getSong.php
+++ SinusBot-Stream/getSong.php
@@ -31,43 +31,71 @@ function checkMetaDataForURL(){
     $checkStrings = array($status['currentTrack']['filename'],
         $status['currentTrack']['album'],
         $status['currentTrack']['artist']
-    );
+        );
 
     foreach ($checkStrings as $str) {
-        if(filter_var($str, FILTER_VALIDATE_URL) !== false){
+        if(validateURL($str) !== false){
             return $str;
         }
     }
     return false;
 }
 
-function resolveURL($url){
+function validateURL($url){
     try{
-    $ch = curl_init($url);
-    curl_setopt($ch,CURLOPT_HEADER,true); // Get header information
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION,false);
-    $header = curl_exec($ch);
-    
-    $fields = explode("\r\n", preg_replace('/\x0D\x0A[\x09\x20]+/', ' ', $header)); // Parse information
-
-    for($i=0;$i<count($fields);$i++)
-    {
-        if(strpos($fields[$i],'Location') !== false)
-        {
-            $url = str_replace("Location: ","",$fields[$i]);
+        if($urlParts = parse_url($url)){
+            if(!isset($urlParts['scheme'])){
+                $url = "http://$url";
+                $urlParts = parse_url($url);
+            }
+            if(!isset($urlParts['host'])){
+                return false;
+            }
+            if(($url = filter_var($url, FILTER_VALIDATE_URL)) === false){
+                return false;
+            }
+        }else{
+            return false;
         }
-    }
-    return $url;
+
+        $file_headers = @get_headers($url);
+        if(!$file_headers || $file_headers[0] == 'HTTP/1.1 404 Not Found') {
+            return false;
+        }
     }catch(Exception $e){
         return false;
     }
+
+    return $url;
 }
 
+function resolveURL($url){
+    try{
+        $ch = curl_init($url);
+        curl_setopt($ch,CURLOPT_HEADER,true); // Get header information
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION,false);
+        $header = curl_exec($ch);
+        $fields = explode("\r\n", preg_replace('/\x0D\x0A[\x09\x20]+/', ' ', $header)); // Parse information
+
+        for($i=0;$i<count($fields);$i++){
+            if(strpos($fields[$i],'Location') !== false){
+               $url = str_replace("Location: ","",$fields[$i]);
+           }
+       }
+       return $url;
+   }catch(Exception $e){
+      return false;   
+  }
+}
+
+
 function getYoutubeID($url){
-    if(strpos($url,"youtube.com/watch?v=") !== false){
-        $startPos = strpos($url, '?v=') + 3;
-        return substr($url, $startPos, 11);
+    if($query = parse_url($url, PHP_URL_QUERY)){
+        if(($startPos = strpos($query, 'v=')) !== false){
+            $startPos += 2;
+            return substr($query, $startPos, 11);
+        }
     }
     return false;
 }
