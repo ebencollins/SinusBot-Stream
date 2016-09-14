cafc933c659058b5664e5d6b05809e5d8996f36b
Zahzi <collinseben@gmail.com>
2016-09-13 21:56:38 -0400

    Fixed bug where no link would be displayed if URL was invalid

--- SinusBot-Stream/getSongURL.php
+++ SinusBot-Stream/getSongURL.php
@@ -1,14 +1,11 @@
 <?php
 error_reporting('E_ERROR');
 include("getSong.php");
-$link = "";
-
+$link = 'https://www.google.com/search?q=' . getTrack();
 if(($urlFromMD = checkMetaDataForURL()) !== false){ //there is a URL
     if(($urlFull = resolveURL($urlFromMD)) !== false){ //it can be resolved to something
         $link = $urlFull;
     }
-}else{ //else, google search
-    $link = 'https://www.google.com/search?q=' . getTrack();
 }
 
 if(!empty(getArtist())) {
