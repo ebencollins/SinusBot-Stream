4a157396feeb7d4f145ca875970552d131169733
Zahzi <collinseben@gmail.com>
2016-12-06 18:04:49 -0500

    Restructured getSongURL.php to maybe fix song link issue

--- SinusBot-Stream/getSongURL.php
+++ SinusBot-Stream/getSongURL.php
@@ -1,19 +1,20 @@
 <?php
 error_reporting('E_ERROR');
 require("getSong.php");
-$link = 'https://www.google.com/search?q=' . getTrack();
-if(($urlFromMD = checkMetaDataForURL()) !== false){ //there is a URL
-    if(($urlFull = resolveURL($urlFromMD)) !== false){ //it can be resolved to something
-    	$link = $urlFull;
-    }
-}
-if(empty(getName()) || getName() == null || getName() == ""){
-	$finalURL = '<p>No song name given. </p>';
+
+if(getTrack() == null || getTrack() == ""){
+	$finalURL = '<p>No song name given.</p>';
 }else{
-	if(!empty(getArtist()) && getArtist() != "") {
-		$finalURL = '<a class="songlink" href="'.$link.'" target="_blank">Song: '.getName().' from ' .getArtist(). '</a>';
+	$link = 'https://www.google.com/search?q=' . getTrack();
+	if(($urlFromMD = checkMetaDataForURL()) !== false){ //there is a URL
+	    if(($urlFull = resolveURL($urlFromMD)) !== false){ //it can be resolved to something
+	    	$link = $urlFull;
+	    }
+	}
+	if(getArtist() != "" && getArtist() != null) {
+		$finalURL = '<a class="songlink" href="'.$link.'" target="_blank">Song: '.getTrack().' from ' .getArtist(). '</a>';
 	} else {
-		$finalURL = '<a class="songlink" href="'.$link.'" target="_blank">Song: '.getName().'</a>';
+		$finalURL = '<a class="songlink" href="'.$link.'" target="_blank">Song: '.getTrack().'</a>';
 	}
 }
 echo $finalURL;
