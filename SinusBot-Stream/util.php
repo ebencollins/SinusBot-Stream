b58b537086158337ebf0a59bc71df4e42a4cf439
Zahzi <collinseben@gmail.com>
2017-12-11 19:23:55 -0500

    support for 0.11.0+; closes #8 & #11

--- SinusBot-Stream/util.php
+++ SinusBot-Stream/util.php
@@ -197,19 +197,7 @@ function returns404($url){
 
 function resolveURL($url){
     try{
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 1);
-        curl_setopt($ch,CURLOPT_HEADER,true); // Get header information
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION,false);
-        $header = curl_exec($ch);
-        $fields = explode("\r\n", preg_replace('/\x0D\x0A[\x09\x20]+/', ' ', $header)); // Parse information
-
-        for($i=0;$i<count($fields);$i++){
-            if(strpos($fields[$i],'Location') !== false){
-             $url = str_replace("Location: ","",$fields[$i]);
-         }
-     }
+     #removed functionality for now. Might need to readd in the future
      return $url;
  }catch(Exception $e){
   return false;   
